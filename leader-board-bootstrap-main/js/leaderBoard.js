c27b6ae235a660172d1c83a6416c80b47a3db6d7
mehesultana <sultanamehe01@gmail.com>
2021-08-20 19:38:02 +0600

    background color change

--- leader-board-bootstrap-main/js/leaderBoard.js
+++ leader-board-bootstrap-main/js/leaderBoard.js
@@ -3,3 +3,4 @@ document.getElementById('top-player').style.color = '#ff0000';
 document.getElementById('top-blog').style.color = '#ff0000';
 
 //top players background
+document.getElementById('top-players').style.backgroundColor = 'rgba(219, 219, 161, 1)';
