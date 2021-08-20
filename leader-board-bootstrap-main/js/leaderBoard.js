e391af4d560cc2346aeb837b950914af1c22c235
mehesultana <sultanamehe01@gmail.com>
2021-08-20 19:56:33 +0600

    add ul

--- leader-board-bootstrap-main/js/leaderBoard.js
+++ leader-board-bootstrap-main/js/leaderBoard.js
@@ -4,3 +4,23 @@ document.getElementById('top-blog').style.color = '#ff0000';
 
 //top players background
 document.getElementById('top-players').style.backgroundColor = 'rgba(219, 219, 161, 1)';
+
+// add ul
+
+const courses = document.getElementsByClassName('course');
+for (const course of courses) {
+    course.addEventListener('click', function (event) {
+        event.target.parentNode.removeChild(event.target);
+    });
+}
+document.getElementById('add-course').addEventListener('click', function () {
+    const li = document.createElement('li');
+    li.innerText = 'The Complete JavaScript Course 2021';
+    li.classList.add('course');
+    const parent = document.getElementById('course-container');
+    parent.appendChild(li);
+});
+
+document.getElementById('course-container').addEventListener('click', function (event) {
+    event.target.parentNode.removeChild(event.target);
+});
