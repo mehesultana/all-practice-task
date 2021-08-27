e2e94c2557193f9fa716762465744032c9202658
mehesultana <sultanamehe01@gmail.com>
2021-08-28 02:21:56 +0600

    load more data

--- api-practice/api.js
+++ api-practice/api.js
@@ -11,3 +11,19 @@ function loadData() {
         .then((response) => response.json())
         .then((data) => console.log(data));
 }
+
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((data) => displayUsers(data));
+}
+
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+}
+
+function displayUsers(data) {
+    console.log(data);
+}
